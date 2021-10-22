8418a0d8116f9bfcc80d9e7f43528976a3a033e7
niezhiyang <nie.zhiyang@immomo.com>
2021-10-22 18:20:49 +0800

    裁剪mp3 完成

--- app/src/main/java/com/nzy/aac/MainActivity.java
+++ app/src/main/java/com/nzy/aac/MainActivity.java
@@ -1,14 +1,124 @@
 package com.nzy.aac;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * 裁剪 mp3 ，
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private String musicPath;
+    private String musicPcmPath;
+    private MusicProcess musicProcess;
+    private String outMp3Path;
+    private TextView mTvProgress;
+    private static final long STAR_TTIME = 10 * 1000 * 1000;
+    private static final long END_TIME = 15 * 1000 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvProgress = findViewById(R.id.tv_progress);
+        checkPermission();
+        // 降噪 也就是 把 异常的波形 去除就可以了
+        // 噪音 和 正常的 波形 去叠加。然后记录 所有的噪音 （固定的已知的），然后去除
+
+        /**
+         * 截取音频，不能直接截取aac ，需要截取 pcm ，然后截取
+         */
+        musicProcess = new MusicProcess(this);
+
+
+    }
+
+    public boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+        }
+        return false;
+    }
+
+    public void start(View view) {
+        File file = new File(Environment.getExternalStorageDirectory(), "AAC");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        File fileMusic = new File(file, "music.mp3");
+        if (fileMusic.exists()) {
+            fileMusic.delete();
+        }
+        File filePcm = new File(file, "out.pcm");
+        if (filePcm.exists()) {
+            filePcm.delete();
+        }
+        File fileOutMp3 = new File(file, "out.mp3");
+        if (fileOutMp3.exists()) {
+            fileOutMp3.delete();
+        }
+        musicPath = fileMusic.getAbsolutePath();
+        musicPcmPath = filePcm.getAbsolutePath();
+        outMp3Path = fileOutMp3.getAbsolutePath();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    copyAssets("music.mp3", musicPath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    // 编码都是 微秒
+                    musicProcess.clip(musicPath, musicPcmPath, outMp3Path, STAR_TTIME, END_TIME, new MusicProcess.CallBack() {
+                        @Override
+                        public void progress(long progress) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    long sum = END_TIME - STAR_TTIME;
+                                    long num = END_TIME - progress;
+
+                                    long percent = num * 100 / sum;
+                                    mTvProgress.setText("进度是："+percent);
+
+
+                                }
+                            });
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+
+    private void copyAssets(String assetsName, String path) throws IOException {
+        AssetFileDescriptor assetFileDescriptor = getAssets().openFd(assetsName);
+        FileChannel from = new FileInputStream(assetFileDescriptor.getFileDescriptor()).getChannel();
+        FileChannel to = new FileOutputStream(path).getChannel();
+        from.transferTo(assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength(), to);
     }
 }
---
+++ app/src/main/java/com/nzy/aac/MusicProcess.java
@@ -0,0 +1,159 @@
+package com.nzy.aac;
+
+import android.content.Context;
+import android.media.AudioFormat;
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author niezhiyang
+ * since 10/20/21
+ */
+public class MusicProcess {
+    private static final String TAG = "MusicProcess";
+    private Context mContext;
+
+    public MusicProcess(Context context) {
+        mContext = context;
+    }
+
+    public void clip(String musicPath, String musicPcmPath,String outMp3Path, long startTime, long endTime,CallBack callBack) throws IOException {
+        Log.i(TAG, "转换开始");
+        // 解封装 封装格式 mp3 ,mp3 封装着 aac
+        MediaExtractor mediaExtractor = new MediaExtractor();
+        // 设置文件路径
+        mediaExtractor.setDataSource(musicPath);
+
+        // 音频流 轨道的index
+        int audioTrack = selectTrack(mediaExtractor);
+        // 定位到 这个音频轨道
+        mediaExtractor.selectTrack(audioTrack);
+
+        // 直接 seekto 到 startTime
+        mediaExtractor.seekTo(startTime, MediaExtractor.SEEK_TO_CLOSEST_SYNC);
+
+        // 轨道信息
+        MediaFormat trackFormat = mediaExtractor.getTrackFormat(audioTrack);
+
+        // 100k
+        int maxufferSize = 100 * 1000;
+        // 有的音频有 ，有的音频没有
+        if (trackFormat.containsKey(MediaFormat.KEY_MAX_INPUT_SIZE)) {
+            maxufferSize = trackFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE);
+        }
+
+        // 设置缓存大小
+        ByteBuffer buffer = ByteBuffer.allocateDirect(maxufferSize);
+        // 通过 MediaFormat.KEY_MIME 这个 key 拿到 编码格式
+        MediaCodec mediaCodec = MediaCodec.createDecoderByType(trackFormat.getString(MediaFormat.KEY_MIME));
+        // 因为需要把aac 转成 pcm 即是解码，flags = 0
+        mediaCodec.configure(trackFormat, null, null, 0);
+
+        // 把 mp3 解压的pcm 放到musicPcmFile文件中
+        File musicPcmFile = new File(musicPcmPath);
+        FileChannel writeChannel = new FileOutputStream(musicPcmFile).getChannel();
+        // 开始解
+        mediaCodec.start();
+        MediaCodec.BufferInfo info = new MediaCodec.BufferInfo();
+
+        while (true) {
+            // 读取数据
+            int index = mediaCodec.dequeueInputBuffer(100_000);
+            if (index > 0) {
+
+                // 拿到时间
+                long sampleTime = mediaExtractor.getSampleTime();
+                Log.e(TAG, index + "---"+sampleTime);
+                callBack.progress(sampleTime);
+                if (sampleTime == -1) {
+                    //     结束
+                    break;
+                } else if (sampleTime < startTime) {
+                    // 下一个数据 Advance to the next sample
+                    mediaExtractor.advance();
+                    continue;
+                } else if (sampleTime > endTime) {
+                    //     结束
+                    break;
+                }
+                // 获取压缩数据 压缩数据大小
+                // 读取数据到bytebuffer中，从offset偏移开始。选定轨道之后可以读取该轨道的数据。
+                info.size = mediaExtractor.readSampleData(buffer, 0);
+                info.presentationTimeUs = sampleTime;
+                info.flags = mediaExtractor.getSampleFlags();
+
+
+                byte[] content = new byte[buffer.remaining()];
+                buffer.get(content);
+
+                // 把这个写到txt 文件中中 以便于我们观察
+                FileUtils.writeContent(content, mContext);
+
+                // 解码
+                ByteBuffer inputBuffer = mediaCodec.getInputBuffer(index);
+                inputBuffer.put(content);
+                mediaCodec.queueInputBuffer(index, 0, info.size, info.presentationTimeUs, info.flags);
+
+                // 获取下一帧数据
+                mediaExtractor.advance();
+            }
+
+            // 解码 拿出来输出数据
+            int outputBufferIndex = mediaCodec.dequeueOutputBuffer(info, 100_000);
+            while (outputBufferIndex >= 0) {
+                ByteBuffer decodeOutputBuffer = mediaCodec.getOutputBuffer(outputBufferIndex);
+                //写到 pcm 文件中
+                writeChannel.write(decodeOutputBuffer);
+                // 释放
+                mediaCodec.releaseOutputBuffer(outputBufferIndex, false);
+                // 下一个数据
+                outputBufferIndex = mediaCodec.dequeueOutputBuffer(info, 100_000);
+            }
+
+        }
+        writeChannel.close();
+        mediaExtractor.release();
+        mediaCodec.stop();
+        mediaCodec.release();
+
+
+        // pcm数据转换成mp3封装格式
+
+        new PcmToWavUtil(44100, AudioFormat.CHANNEL_IN_STEREO,
+                2, AudioFormat.ENCODING_PCM_16BIT).pcmToWav(musicPcmFile.getAbsolutePath()
+                , outMp3Path);
+        Log.i(TAG, "转换完毕");
+
+
+    }
+
+    private int selectTrack(MediaExtractor mediaExtractor) {
+        // 轨道数量，就是有多少个文件 比如一个zip里面有很多文件
+        // 每个轨道都有配置信息
+        int trackCount = mediaExtractor.getTrackCount();
+        for (int i = 0; i < trackCount; i++) {
+            // 就是一个配置信息
+            MediaFormat format = mediaExtractor.getTrackFormat(i);
+            String mime = format.getString(MediaFormat.KEY_MIME);
+            // 如果是 video/ 开头 那就是视频
+            // 如果是音频
+            if (mime.startsWith("audio/")) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public interface CallBack{
+        void progress(long progress);
+    }
+}
